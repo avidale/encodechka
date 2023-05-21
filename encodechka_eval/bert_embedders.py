8cb338f4ad2babf1d41b0883a9da255e585b535d
david <dale.david@mail.ru>
2023-05-21 13:09:52 +0200

    fix BERT mean pooling; increase the number of embedders

--- encodechka_eval/bert_embedders.py
+++ encodechka_eval/bert_embedders.py
@@ -25,25 +25,27 @@ def embed_bert_cls2(text, model, tokenizer, max_length=128):
     return embeddings[0].cpu().numpy()
 
 
-def mean_pooling(model_output, attention_mask, norm=True):
+def mean_pooling(model_output, attention_mask, norm=False):
     token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    sum_embeddings = torch.sum(token_embeddings ** 2 * input_mask_expanded, 1)
-    sum_mask = torch.clamp(input_mask_expanded.sum([1]), min=1e-9)
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     sums = sum_embeddings / sum_mask
     if norm:
         sums = torch.nn.functional.normalize(sums)
     return sums
 
 
-def embed_bert_pool(text, model, tokenizer, max_length=128):
+def embed_bert_pool(text, model, tokenizer, max_length=128, norm=False):
     encoded_input = tokenizer(text, padding=True, truncation=True, max_length=max_length, return_tensors='pt')
     encoded_input = {k: v.to(model.device) for k, v in encoded_input.items()}
     with torch.no_grad():
         model_output = model(**encoded_input)
 
-    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
-    return {'mean': sentence_embeddings[0].cpu().numpy()}
+    embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+    if norm:
+        embeddings = torch.nn.functional.normalize(embeddings)
+    return {'mean': embeddings[0].cpu().numpy()}
 
 
 def embed_bert_both(text, model, tokenizer, max_length=128):
@@ -51,9 +53,22 @@ def embed_bert_both(text, model, tokenizer, max_length=128):
     t = {k: v.to(model.device) for k, v in t.items()}
     with torch.no_grad():
         model_output = model(**t)
-    e1 = torch.nn.functional.normalize(model_output.last_hidden_state[:, 0, :])
-    e2 = mean_pooling(model_output, t['attention_mask'])
-    return {'cls': e1[0].cpu().numpy(), 'mean': e2[0].cpu().numpy()}
+    cls_emb = model_output.last_hidden_state[:, 0, :]
+    cls_emb_norm = torch.nn.functional.normalize(cls_emb)
+    mean_emb = mean_pooling(model_output, t['attention_mask'])
+    mean_emb_norm = torch.nn.functional.normalize(mean_emb)
+    result = {
+        'cls': cls_emb[0].cpu().numpy(),
+        'mean': mean_emb[0].cpu().numpy(),
+        'cls_norm': cls_emb_norm[0].cpu().numpy(),
+        'mean_norm': mean_emb_norm[0].cpu().numpy(),
+    }
+    if hasattr(model_output, 'pooler_output') and model_output.pooler_output is not None:
+        po = model_output.pooler_output
+        po_normed = torch.nn.functional.normalize(po)
+        result['pooler'] = po[0].cpu().numpy()
+        result['pooler_norm'] = po_normed[0].cpu().numpy()
+    return result
 
 
 def get_word_vectors_with_bert(words, model, tokenizer, return_raw=False):
